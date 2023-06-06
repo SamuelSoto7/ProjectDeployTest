bffb9efc5b6bf0ebfccda1fb1361029f1a696ba4
Sam <samueru7@gmail.com>
2023-06-06 01:05:42 -0500

    deploi10

--- projectdeploytest/settings.py
+++ projectdeploytest/settings.py
@@ -141,19 +141,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = './static/'
+STATIC_URL = '/static/'
 # Following settings only make sense on production and may break development environments.
 if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
     
-STATICFILES_DIRS = (os.path.join(BASE_DIR, './static/css/'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, '/static/css/'),)
 
-MEDIA_URL='./media/'
-MEDIA_ROOT=os.path.join(BASE_DIR,"./media")
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,"/media")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
