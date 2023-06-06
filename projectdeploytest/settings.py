27e758164dc4b29ad7e62bf70af2eb1258ce6ddf
Sam <samueru7@gmail.com>
2023-06-06 02:09:50 -0500

    deploi11

--- projectdeploytest/settings.py
+++ projectdeploytest/settings.py
@@ -150,7 +150,12 @@ if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
     
-STATICFILES_DIRS = (os.path.join(BASE_DIR, '/static/css/'),)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static', 'appname'),
+)
+
+ 
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,"/media")
