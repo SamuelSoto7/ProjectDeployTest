cacb30e7c7cdec18ba67615baa847f332c700a18
Sam <samueru7@gmail.com>
2023-06-06 00:27:11 -0500

    deploi6

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'knowledge_people.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectdeploytest.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
